eabba816191eec85a4a9c2797810d10b2e3454d0
Eugen Blattner <eugen.blattner@mayflower.de>
2021-01-31 18:22:32 +0100

    further adjust of container registry

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./mvnw package -Dmaven.test.skip=true &&
-docker build -f src/main/docker/Dockerfile.jvm -t quarkus/blog . &&
-docker tag quarkus/blog blanorama/blog:1.0.0 &&
-docker push ghcr.io/blanorama/blog:1.0.0
+docker build -f src/main/docker/Dockerfile.jvm -t ghcr.io/quarkus/blog . &&
+docker tag ghcr.io/quarkus/blog ghcr.io/blanorama/blog &&
+docker push ghcr.io/blanorama/blog
